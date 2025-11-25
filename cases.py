4a47db1bcd04fc693f3662bbd7acea8b26ebce9a
Maximilian Pierzyna <m.pierzyna@tudelft.nl>
2025-11-25 17:34:40 +0100

    update GABLS case to return objects for model

--- cases.py
+++ cases.py
@@ -7,93 +7,84 @@ import matplotlib.pyplot as plt
 from scm import consts
 from scm.grid import StaggeredGrid
 from scm.interfaces import ProgVars, TransientForcing
+from scm.closures.mynn import ProgVarsMYNN
 
 
-def get_gables1(Nz: int = 128, plot: bool = False, random_seed: int = 0) -> Tuple[Dict, Dict, Dict, Dict]:
-    # Grid
-    H = 400
-    zh = jnp.linspace(0, H, Nz + 1)
-    z = 0.5 * (zh[:-1] + zh[1:])
+def get_gabls1(
+    Nz: int = 128, plot: bool = False, random_seed: int = 0
+) -> Tuple[StaggeredGrid, ProgVarsMYNN, TransientForcing]:
+    ## Grid
+    grid = StaggeredGrid(H=400, Nz=Nz)
     z_inv = 100
 
-    # Coriolis parameter
-    f = 1.39e-4  # 1/s, ~73 deg latitude
-
+    ## Forcing
     # Geostrophic wind
-    ug = jnp.ones_like(z) * 8.0  # m/s
-    vg = jnp.zeros_like(z)  # m/s
+    ug = jnp.ones(Nz) * 8.0  # m/s
+    vg = jnp.zeros(Nz)  # m/s
 
     # Surface temperature forcing
     th_sfc_0 = 263.5  # K
     th_sfc_fn = lambda t_s: th_sfc_0 - 0.25 * t_s / (60 * 60)  # K, 0.25 K per hour cooling
 
+    forcing = TransientForcing(
+        u_geo=lambda t: ug,
+        v_geo=lambda t: vg,
+        f_c=1.39e-4,  # 1/s, ~73 deg latitude
+        # todo: implement forcing with sfc temp. Also, remove 1/10 factor below. This is a bug in MOST somewhere.
+        w_th_s=lambda t: jnp.array(-0.08 / 10),  # K m/s, constant surface cooling
+        w_q_s=lambda t: jnp.array(0.0),  # g/kg m/s
+    )
+
+    ## Initial conditions
     # Initial wind profile
-    u = jnp.copy(ug).at[0].set(0.0)  # geostrophic wind but with no-slip at surface
+    u = jnp.copy(ug)  # .at[0].set(0.0)  # geostrophic wind but with no-slip at surface
     v = jnp.copy(vg)
 
     # Initial temperature
-    th = jnp.ones_like(z) * 265.0  # K
-    th = jnp.where(z > z_inv, th + 0.01 * (z - z_inv), th)  # capping inversion
+    th = jnp.ones(Nz) * 265.0  # K
+    th = jnp.where(grid.z > z_inv, th + 0.01 * (grid.z - z_inv), th)  # capping inversion
     th = jnp.where(
-        z < 50, th + 0.1 * jax.random.normal(key=jax.random.key(random_seed), shape=(Nz,)), th
+        grid.z < 50, th + 0.1 * jax.random.normal(key=jax.random.key(random_seed), shape=(Nz,)), th
     )  # random 0.1K perturbation near surface
 
     # Initial TKE
-    tke = jnp.zeros_like(z)
-    tke = jnp.where(z < 250, 0.4 * (1 - z / 250) ** 3, tke)  # m^2 s^-2
+    tke = jnp.zeros(grid.Nz)
+    tke = jnp.where(grid.z < 250, 0.4 * (1 - grid.z / 250) ** 3, tke)  # m^2 s^-2
 
-    # Surface model
-    z0m = z0h = 0.1  # m, roughness lengths for momentum and heat
-    beta_m = 4.8  # MOST momentum stability coefficent
-    beta_h = 7.8  # MOST heat stability coefficient
+    init = ProgVarsMYNN(u=u, v=v, thv=th, q_sq=2 * tke)
 
-    grid = {
-        "z": z,  # Nz is len
-        "zh": zh,  # H is zh[-1]
-    }
-
-    forcing = {
-        "ug": ug,
-        "vg": vg,
-        "f": f,
-        "th_sfc_0": th_sfc_0,
-        "th_sfc_fn": th_sfc_fn,
-    }
-
-    init = {
-        "u": u,
-        "v": v,
-        "th": th,
-        "tke": tke,
-    }
-
-    lsm = {
-        "z0m": z0m,
-        "z0h": z0h,
-        "beta_m": beta_m,
-        "beta_h": beta_h,
-    }
+    ## Surface model (not in use)
+    # z0m = z0h = 0.1  # m, roughness lengths for momentum and heat
+    # beta_m = 4.8  # MOST momentum stability coefficent
+    # beta_h = 7.8  # MOST heat stability coefficient
+    #
+    # lsm = {
+    #     "z0m": z0m,
+    #     "z0h": z0h,
+    #     "beta_m": beta_m,
+    #     "beta_h": beta_h,
+    # }
 
     if plot:
         # Initial conditions
         fig, axarr = plt.subplots(ncols=3, figsize=(8, 2), sharey="row", layout="constrained")
-        axarr[0].plot(u, z, label="u")
-        axarr[0].plot(v, z, label="v")
+        axarr[0].plot(u, grid.z, label="u")
+        axarr[0].plot(v, grid.z, label="v")
         axarr[0].set_xlabel("Wind (m/s)")
         axarr[0].set_ylabel("Height (m)")
         axarr[0].legend()
 
-        axarr[1].plot(th, z)
+        axarr[1].plot(th, grid.z)
         axarr[1].set_xlabel("Potential Temperature (K)")
 
-        axarr[2].plot(tke, z)
+        axarr[2].plot(tke, grid.z)
         axarr[2].set_xlabel("TKE (m$^2$/s$^2$)")
         fig.show()
 
         # Forcing
         fig, axarr = plt.subplots(ncols=2, figsize=(8, 2), width_ratios=[1, 3], layout="constrained")
-        axarr[0].plot(ug, z, label="ug")
-        axarr[0].plot(vg, z, label="vg")
+        axarr[0].plot(ug, grid.z, label="ug")
+        axarr[0].plot(vg, grid.z, label="vg")
         axarr[0].set_xlabel("Geostrophic Wind (m/s)")
         axarr[0].legend()
 
@@ -104,11 +95,14 @@ def get_gables1(Nz: int = 128, plot: bool = False, random_seed: int = 0) -> Tupl
 
         fig.show()
 
-    return grid, init, forcing, lsm
+    return grid, init, forcing
 
 
-def get_ysu(Nz: int = 138, plot: bool = False) -> Tuple[StaggeredGrid, ProgVars, TransientForcing]:
-    """Initial conditions and forcing from HND06"""
+def get_ysu(Nz: int = 138, plot: bool = False, debug_dt: float = 0) -> Tuple[StaggeredGrid, ProgVars, TransientForcing]:
+    """Initial conditions and forcing from HND06
+
+    Use debug_dt to shift the time of the forcing functions for debugging purposes.
+    """
     # Grid
     grid = StaggeredGrid(H=2750, Nz=Nz)
     z_inv = 500.0  # Inversion height in m
@@ -133,7 +127,7 @@ def get_ysu(Nz: int = 138, plot: bool = False) -> Tuple[StaggeredGrid, ProgVars,
     @jax.jit
     def _shfx(t_s: jnp.ndarray) -> jnp.ndarray:
         """Surface heat flux as function of time in seconds after simulation begin."""
-        t_h = t_s / 3600.0  # time in hours
+        t_h = (t_s + debug_dt) / 3600.0  # time in hours
         shfx = jnp.sin((t_h + 2) * jnp.pi / 12) * 400  # W/m2 = J/(s m2)
         shfx = shfx / (consts.rho_0 * consts.cp)  # convert to (K m/s)
         return shfx
@@ -141,7 +135,7 @@ def get_ysu(Nz: int = 138, plot: bool = False) -> Tuple[StaggeredGrid, ProgVars,
     @jax.jit
     def _lhfx(t_s: jnp.ndarray) -> jnp.ndarray:
         """Surface latent heat flux as function of time in seconds after simulation begin."""
-        t_h = t_s / 3600.0  # time in hours
+        t_h = (t_s + debug_dt) / 3600.0  # time in hours
         lhfx = jnp.sin(t_h * jnp.pi / 12) * 200  # W/m2
         lhfx = lhfx / 1225  # convert to (g/kg m/s)  # todo: correct like this?
         return lhfx
@@ -228,6 +222,6 @@ def get_ekman(Nz: int = 100, plot: bool = False):
 
 
 if __name__ == "__main__":
-    # get_gables1(plot=True)
+    get_gabls1(plot=True)
     # get_ekman(plot=True)
-    get_ysu(plot=True)
+    # get_ysu(plot=True, debug_dt=0 * 60)
