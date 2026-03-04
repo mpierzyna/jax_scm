d1d67627c7daf3ccdd26ce5d833eb446dd30be9e
Maximilian Pierzyna <m.pierzyna@tudelft.nl>
2026-03-04 14:56:56 +0100

    Bugfix: start and end time of simulation now set properly

--- src/scm/mynn/io.py
+++ src/scm/mynn/io.py
@@ -75,6 +75,7 @@ def sim_from_ds(ds: xr.Dataset, **override) -> Simulation[ProgVarsMYNN, DiagVars
 
     ## Meta data
     name = override.get("name", ds.attrs.get("name", "loaded_simulation"))
+    t_start_s, t_end_s = ds["_t_s"].values[[0, -1]]
 
     return Simulation(
         name=name,
@@ -82,6 +83,6 @@ def sim_from_ds(ds: xr.Dataset, **override) -> Simulation[ProgVarsMYNN, DiagVars
         init=init,
         forcing=forcing,
         mo_settings=mo_settings,
-        t_start_s=ds.attrs["t_start_s"],
-        t_end_s=ds.attrs["t_end_s"],
+        t_start_s=t_start_s,
+        t_end_s=t_end_s,
     )
